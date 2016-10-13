0946bf5033299c91bb120cab8631ab18ab918394
Raj Tomar <rajtomar50@gmail.com>
2016-10-13 14:58:35 +0530

    first commit of spring-hibernate using maven

---
+++ spring-hibernate-maven/src/main/java/com/raj/beans/EmployeeBean.java
@@ -0,0 +1,74 @@
+package com.raj.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class EmployeeBean{
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name="firstname")
+	private String firstName;
+	
+	@Column(name="lastname")
+	private String lastName;
+	
+	@Column(name="designation")
+	private String designation;
+	
+	@Column(name="companyname")
+	private String companyName;
+
+	public EmployeeBean() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+}
---
+++ spring-hibernate-maven/src/main/java/com/raj/employee/daoImpl/EmployeeDaoImpl.java
@@ -0,0 +1,96 @@
+package com.raj.employee.daoImpl;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.raj.beans.EmployeeBean;
+import com.raj.employee.dao.EmployeeDao;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao{
+	
+	@Autowired
+	 private SessionFactory sessionFactory;
+	 private Session session = null;
+	 private Transaction tx = null;
+	 
+	 private static Logger logger = Logger.getLogger(EmployeeDaoImpl.class);
+
+
+	public String saveOrUpdateEmployee(EmployeeBean employee) {
+		logger.info("saveOrUpdateEmployee daoImpl");
+		String status = "0";
+		try {
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+			if(null == employee.getId()){
+				session.save(employee);
+				tx.commit();
+				status = "1";
+			}else{
+				session.update(employee);
+				tx.commit();
+				status = "1";
+			}
+		} catch (Exception e) {
+			logger.error("Exception: "+e.getMessage());
+		}
+		return status;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public List<EmployeeBean> getAllEmployee() {
+		logger.info("getAllEmployee daoImpl");
+		List<EmployeeBean> list = null;
+		try{
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+			Query query = session.createQuery("From EmployeeBean");
+			list = query.getResultList();
+			logger.info("Total Employee: "+list.size());
+		}
+		catch(Exception e){
+			logger.error("Exception: "+e.getMessage());
+		}
+		return list;
+	}
+
+	public EmployeeBean getEmployeeById(Integer id) {
+		logger.info("getEmployeeById daoImpl");
+		EmployeeBean bean = null;
+		try {
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+			bean = session.get(EmployeeBean.class, id);
+		} catch (Exception e) {
+			logger.error("Exception: "+e.getMessage());
+		}
+		return bean;
+	}
+
+	public String deleteEmployee(Integer id) {
+		logger.info("deleteEmployee daoImpl");
+		String status = "0";
+		try {
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+			EmployeeBean bean = session.load(EmployeeBean.class, id);
+			if(null != bean){
+				session.delete(bean);
+				tx.commit();
+				status = "1";
+			}
+		} catch (Exception e) {
+			logger.error("Exception: "+e.getMessage());
+		}
+		return status;
+	}
+
+}
